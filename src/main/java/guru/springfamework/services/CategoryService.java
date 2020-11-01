656c0fb77e04c4f74048090429ed0c71ea40eb5d
hlaingwintunn <hlaingwintunn@gmail.com>
2020-11-01 13:06:05 +0800

    adding Category Service

---
+++ src/main/java/guru/springfamework/services/CategoryService.java
@@ -0,0 +1,14 @@
+package guru.springfamework.services;
+
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.domain.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+
+}
