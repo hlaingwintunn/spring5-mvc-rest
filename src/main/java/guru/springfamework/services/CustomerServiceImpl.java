27325aa4483f612bec5580cf64fb0d6e47f3c494
hlaingwintunn <hlaingwintunn@gmail.com>
2020-11-01 19:06:24 +0800

    adding controller method for patch

--- src/main/java/guru/springfamework/services/CustomerServiceImpl.java
+++ src/main/java/guru/springfamework/services/CustomerServiceImpl.java
@@ -68,7 +68,9 @@ public class CustomerServiceImpl implements CustomerService {
                     if(customerDTO.getLastname() != null){
                         customer.setLastname(customerDTO.getLastname());
                     }
-                    return customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+                    CustomerDTO returnDto =  customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+                    returnDto.setCustomerUrl("/api/v1/customer/" + id);
+                    return returnDto;
                 }).orElseThrow(RuntimeException::new); //todo implement better exception handling;
     }
 
