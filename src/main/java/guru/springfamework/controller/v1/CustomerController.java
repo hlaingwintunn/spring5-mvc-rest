d73d0bb414c30fc7d08a75a84a077f920a62bd84
hlaingwintunn <hlaingwintunn@gmail.com>
2020-11-07 17:31:59 +0800

    Customized endpoint docs

--- src/main/java/guru/springfamework/controller/v1/CustomerController.java
+++ src/main/java/guru/springfamework/controller/v1/CustomerController.java
@@ -3,11 +3,14 @@ package guru.springfamework.controller.v1;
 import guru.springfamework.api.v1.model.CustomerDTO;
 import guru.springfamework.api.v1.model.CustomerListDTO;
 import guru.springfamework.services.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@Api(description = "This is Customer Controller")
 @RestController
 @RequestMapping(CustomerController.BASE_URL)
 public class CustomerController {
@@ -19,6 +22,7 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @ApiOperation(value = "This will get a lists of customer", notes = "This is some note about API")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public CustomerListDTO getAllCustomers() {
